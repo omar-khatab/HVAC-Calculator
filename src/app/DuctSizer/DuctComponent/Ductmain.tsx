7c300f8180ab5bb625d9d41e61e494d91f887306
omar-khatab <omaressam0870@gmail.com>
2026-09-18 12:37:57 +0300

    custmoize the sizes of ducts in recommendations

--- src/app/DuctSizer/DuctComponent/Ductmain.tsx
+++ src/app/DuctSizer/DuctComponent/Ductmain.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import MainOutput from "./Outputs/MainOutput";
 import DuctCalc from "./Calculations/DuctCalc";
 import DuctFormula from "./Calculations/DuctFormula";
+import DuctRecommendations from "./Recommendations/DuctRecommendations";
 
 export default function Ductmain() {
 const [inputs, setInputs] = useState<DuctSizerInputs>({
@@ -19,6 +20,7 @@ const [inputs, setInputs] = useState<DuctSizerInputs>({
       <DuctCalc inputs={inputs} setInputs={setInputs}/>
       <DuctFormula inputs={inputs} result = {result}/>
       <MainOutput result ={result} inputs = {inputs}/>
+      <DuctRecommendations result = {result} />
     </>
   )
 }
